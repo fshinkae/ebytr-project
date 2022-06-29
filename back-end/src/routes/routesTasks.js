9290b9670381fa8f40176db506150c19899b1661
fshinkae <felipeshinkae97@gmail.com>
2022-06-29 14:33:17 -0400

    Feat: Created Routes to get all tasks and tested successfully

--- back-end/src/routes/routesTasks.js
+++ back-end/src/routes/routesTasks.js
@@ -2,6 +2,7 @@ const routerTask = require('express').Router();
 const controllerTask = require('../controller/controllerTasks');
 
 routerTask.post('/', controllerTask.create);
+routerTask.get('/', controllerTask.read);
 
 module.exports = {
   routerTask,
