0684bd82f365d5b591f53fae0fb44474c8bd1eca
fshinkae <felipeshinkae97@gmail.com>
2022-06-29 14:42:34 -0400

    Feat: Created update function in controller

--- back-end/src/controller/controllerTasks.js
+++ back-end/src/controller/controllerTasks.js
@@ -11,7 +11,15 @@ const read = async (req, res) => {
   res.status(200).json(allTasks);
 };
 
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  const updateTask = await serviceTask.serviceUpdate(id, body);
+  res.status(200).json(updateTask);
+};
+
 module.exports = {
   create,
   read,
+  update,
 };
