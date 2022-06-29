2f91a3d8a69bce13d6eae475e11496878a72213a
fshinkae <felipeshinkae97@gmail.com>
2022-06-29 14:47:58 -0400

    Feat: Created Routes to update tasks and tested successfully

--- back-end/src/routes/routesTasks.js
+++ back-end/src/routes/routesTasks.js
@@ -3,6 +3,7 @@ const controllerTask = require('../controller/controllerTasks');
 
 routerTask.post('/', controllerTask.create);
 routerTask.get('/', controllerTask.read);
+routerTask.put('/:id', controllerTask.update);
 
 module.exports = {
   routerTask,
