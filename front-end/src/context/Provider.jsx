39be9d9cc90a41ab35dda280f9f6f657ebc24b95
fshinkae <felipeshinkae97@gmail.com>
2022-07-04 09:39:11 -0400

    feat: created context and provider files

---
+++ front-end/src/context/Provider.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import Context from './Context';
+
+export default function Provider({ children }) {
+  const [task, setTask] = useState({});
+
+  const stateGlobal = useMemo(() => ({
+    task,
+    setTask,
+  }), []);
+
+  return (
+    <Context.Provider value={stateGlobal}>
+      { children }
+    </Context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
