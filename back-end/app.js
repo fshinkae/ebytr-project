d90cacec81570f00ca1c2a8fab261b30515e17e7
fshinkae <felipeshinkae97@gmail.com>
2022-07-04 13:10:58 -0400

    Fix: added cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const { routerTask } = require('./src/routes/routesTasks');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${process.env.PORT} </h1>`));
+// app.get('/', (req, res) => res.send(`<h1> Executando na porta: ${process.env.PORT} </h1>`));
 
 // Função para testes
 // app.get('/', (_request, response) => {
